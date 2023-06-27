40269dd11efd4c04733bb7926826af1a63df1584
Breidy <bjjaime@misena.edu.co>
2023-06-27 10:24:53 -0500

    dashboard funcional provee

--- dashboard.php
+++ dashboard.php
@@ -40,63 +40,113 @@ if (isset($_GET['login']) && $_GET['login'] === 'success') {
 </head>
 
 <body>
-    <div class="container">
-        <div class="cuadro">
-            <ul>
-                <li>
-                    <a href="#">
-                        <span class="icon"><ion-icon name="logo-apple"></ion-icon></span>
-                        <span class="title">Piel de seda</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span class="icon"><ion-icon name="person-circle-outline"></ion-icon></span>
-                        <span class="title">Usuario</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span class="icon"><ion-icon name="briefcase-sharp"></ion-icon></span>
-                        <span class="title">Productos</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="informes.php">
-                        <span class="icon"><ion-icon name="dice-sharp"></ion-icon></span>
-                        <span class="title">informes</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="viewprovee/verproveedor.php">
-                        <span class="icon"><ion-icon name="dice-sharp"></ion-icon></span>
-                        <span class="title">Provedores</span>
-                    </a>
-                </li>
-            </ul>
+    <div class="informe">
+        <div class="cont-dash-btn">
+            <div class="cuadro">
+                <ul>
+                    <li>
+                        <div class="logo-name">
+                            <div class="logo-image">
+                                <img src="../img/clouds (1).png" alt="logo">
+                            </div>
+                            <span class="logo_name">PIEL DE SEDA</span>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
+                            <span class="title">Inicio</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="icon"><ion-icon name="person-circle-outline"></ion-icon></span>
+                            <span class="title">Usuario</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="icon"><ion-icon name="briefcase-sharp"></ion-icon></span>
+                            <span class="title">Producto</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="../informes.php">
+                            <span class="icon"><ion-icon name="dice-sharp"></ion-icon></span>
+                            <span class="title">Factura</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="viewprovee/verproveedor.php">
+                            <span class="icon"><ion-icon name="dice-sharp"></ion-icon></span>
+                            <span class="title">Proveedor</span>
+                        </a>
+                    </li>
+                    <li class="logout">
+                        <a href="#">
+                            <span class="icon"><ion-icon name="dice-sharp"></ion-icon></span>
+                            <span class="title">Logout</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-        <!--Main-->
-        <div class="main">
-            <div class="top">
-                <div class="toggle">
-                    <ion-icon name="menu-sharp"></ion-icon>
-                </div>
-                <!--Buscar-->
-                <div class="buscar">
+
+        <div class="cont-dash-informe">
+            <!-- <div class="top">
+                 <div class="toggle">
+                    <ion-icon name="grid-sharp"></ion-icon>
+                </div> -->
+            <!--Buscar-->
+            <!-- <div class="buscar">
                     <label>
                         <input type="text" placeholder="Buscar">
-                        <ion-icon name="search-sharp"></ion-icon> 
+                        <ion-icon name="search-sharp"></ion-icon>
                     </label>
-                </div>
-                <!-- cansona -->
-                <!--user dasboard-->
-                <div class="user" alt="">
-                    <img src="WhatsApp Image 2023-05-30 at 2.30.10 PM.jpeg" alt="">
+                </div> -->
+
+                <div class="registrar">
+    <div class="logo-container">
+        <img src="../img/cf4d2bbf9bb991e42540b1eb55d2411c.png" alt="Logo de proveedores" class="logo">
+    </div>
+    <h1 class="title">PROVEEDORES</h1>
+</div>
+
+                <div>
+                    <div class="cuadrodeclase">
+                        <div class="clasesita">
+                         
+                        </div>
+                    </div>
+                    <!-- <a class="btn btn-primary" href="registro2.php" role="button">Registra nuevo proveedor</a> -->
                 </div>
             </div>
         </div>
     </div>
+    <!-- Incluye las bibliotecas de jQuery, DataTables y Buttons -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.print.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.colVis.min.js"></script>
+
+    <!-- Incluye las bibliotecas adicionales para PDF y Excel -->
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.pdf.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.excel.min.js"></script>
+
+    <!-- Incluye las bibliotecas de jQuery, DataTables y Buttons -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.6.0/jszip.min.js"></script>
+
+    <script src="lib/xlsx.full.min.js"></script>
+    <script src="../botonesjs/boton.js"></script>
+
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
